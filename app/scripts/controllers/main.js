01791d8084b5dfe9917ded362169296feec43b11
Daniel Codarcea <daniel.codarcea@endava.com>
2015-05-22 16:51:49 +0300

    Generated scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cloudEApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cloudEApp
+ */
+angular.module('cloudEApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
