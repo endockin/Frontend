1a8f10fe75b244fa132694535e4af338d7c5c46a
Daniel Codarcea <denial@192-168-0-102.rdsnet.ro>
2015-05-30 23:17:53 +0300

    Homepage mobile + navigation active states

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,27 +9,34 @@
  * Main module of the application.
  */
 angular
-  .module('VMFactoryApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('VMFactoryApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .controller('HomeController', function ($scope, $location) {
+        $scope.menuClass = function (page) {
+            var current = $location.path().substring(1);
+            console.log(page,current);
+            return page === current ? "active" : "";
+        };
+    });
