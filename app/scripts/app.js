e9aaf28645efd37cd0c85bf09bd2fdc13443a54e
Daniel Codarcea <denial@192-168-0-102.rdsnet.ro>
2015-06-07 08:23:16 +0300

    Started admin views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,6 @@ angular
         'ngCookies',
         'ngMessages',
         'ngResource',
-        'ngRoute',
         'ngSanitize',
         'ngTouch',
         'ui.router',
@@ -23,58 +22,19 @@ angular
     ])
     .config(function ($stateProvider, $urlRouterProvider) {
         // For any unmatched url, redirect to /state1
-        $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/presentation');
         $stateProvider
-            .state('/', {
-                url: '/',
-                templateUrl: 'views/main.html'
-            })
-            .state('/help', {
-                url: '/help',
-                templateUrl: 'views/help.html'
-            })
-            .state('/contact', {
-                url: '/contact',
-                templateUrl: 'views/contact.html'
-            })
-            .state('/features', {
-                url: '/features',
-                templateUrl: 'views/features.html'
-            })
+            .state('presentation', {templateUrl: 'views/presentation.html', controller: 'HomeCtrl'})
+            .state('presentation.main', {url: '/presentation', templateUrl: 'views/presentation/main.html'})
+            .state('presentation.help', {url: '/help',templateUrl: 'views/presentation/help.html'})
+            .state('presentation.contact', {url: '/contact',templateUrl: 'views/presentation/contact.html'})
+            .state('presentation.signup', {url: '/signup',templateUrl: 'views/presentation/signup.html'})
+            .state('presentation.features', {url: '/features',templateUrl: 'views/presentation/features.html'})
+            .state('user', {templateUrl: 'views/user.html', controller: 'UserCtrl'})
+            .state('user.dashboard', {url:'/dashboard', templateUrl: 'views/user/dashboard.html'})
+            .state('user.createservice', {url:'/createservice', templateUrl: 'views/user/create-service.html'})
     })
-    .controller('HomeController', function ($scope, $location, ngDialog) {
-//        // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
-//        if (!String.prototype.trim) {
-//            (function () {
-//                // Make sure we trim BOM and NBSP
-//                var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
-//                String.prototype.trim = function () {
-//                    return this.replace(rtrim, '');
-//                };
-//            })();
-//        }
-//
-//        [].slice.call(document.querySelectorAll('input.input__field')).forEach(function (inputEl) {
-//            // in case the input is already filled..
-//            if (inputEl.value.trim() !== '') {
-//                classie.add(inputEl.parentNode, 'input--filled');
-//            }
-//
-//            // events:
-//            inputEl.addEventListener('focus', onInputFocus);
-//            inputEl.addEventListener('blur', onInputBlur);
-//        });
-//
-//        function onInputFocus(ev) {
-//            classie.add(ev.target.parentNode, 'input--filled');
-//        }
-//
-//        function onInputBlur(ev) {
-//            if (ev.target.value.trim() === '') {
-//                classie.remove(ev.target.parentNode, 'input--filled');
-//            }
-//        }
-
+    .controller('HomeCtrl', function ($scope, $location, ngDialog) {
 
         $scope.menuClass = function (page) {
             var current = $location.path().substring(1);
@@ -83,8 +43,14 @@ angular
 
         $scope.showLogin = function () {
             ngDialog.open({
-                template: 'views/login.html'
+                template: 'views/presentation/login.html'
             });
         };
 
+    })
+    .controller('UserCtrl', function($scope, $location){
+        $scope.menuClass = function (page) {
+            var current = $location.path().substring(1);
+            return page === current ? 'active' : '';
+        };
     });
